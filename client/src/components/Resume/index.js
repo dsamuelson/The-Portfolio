4724303fc6f4bad0b62d7efb681fd8cdfa448db6
David Samuelson <linksn.1fan@gmail.com>
2022-11-07 16:57:17 -0700

    update look

--- client/src/components/Resume/index.js
+++ client/src/components/Resume/index.js
@@ -5,25 +5,26 @@ function Resume() {
         <section className="resume-cont">
 
             <h2> Proficiencies </h2>
-            <h3>Front End</h3>
+            <h3>Front End (In Brief)</h3>
             <ul>
                 <li>HTML5, CSS, Javascript</li>
                 <li>React</li>
                 <li>Redux</li>
-                <li>Apollo Server</li>
+                <li>Apollo Server and GraphQL</li>
                 <li>Stripe</li>
+                <li>And More...</li>
             </ul>
-            <h3>Backend</h3>
+            <h3>Backend (In Brief)</h3>
             <ul>
-                <li>Node.js</li>
+                <li>Node.js (MERN Stack in general)</li>
                 <li>Handlebars</li>
                 <li>SQL</li>
                 <li>MongoDB</li>
                 <li>Express.js</li>
-                <li>MERN Stack in general</li>
                 <li>GraphQL</li>
+                <li>And More...</li>
             </ul>
-            <a href={require("../../assets/pdf/resume.pdf")} download>Click to Download Resume</a>
+            <a href={require("../../assets/pdf/resume.pdf")} className='dlResume' download>Click to Download Resume</a>
         </section>
     )
 }
